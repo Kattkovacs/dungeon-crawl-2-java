704ec2d6a43d98c6c45acb2ac68a2d65bc3aaf56
attila-h <hauth.attila@gmail.com>
2020-07-01 01:00:18 +0200

    add chooseable design for maps

--- src/main/java/com/codecool/dungeoncrawl/logic/CellType.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/CellType.java
@@ -3,15 +3,23 @@ package com.codecool.dungeoncrawl.logic;
 public enum CellType {
     EMPTY("empty"),
     FLOOR("floor"),
-    WALL("wall");
+    WALL("wall"),
+    COLUMN("column"),
+    DECOR("decor"),
+    EXIT("exit");
 
     private final String tileName;
+    private int style;
 
     CellType(String tileName) {
         this.tileName = tileName;
     }
 
     public String getTileName() {
-        return tileName;
+        return tileName + getStyle();
     }
+
+    public int getStyle() { return style; }
+
+    public void setStyle(int style) { this.style = style; }
 }
